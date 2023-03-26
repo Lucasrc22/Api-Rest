e718b11e03b7d075a7b63cad10327bfe685f4bf1
Lucas Ribeiro Costa <lucasrios22@hotmail.com>
2023-03-26 09:35:48 -0300

    ts para js

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,10 @@
-console.log('Hello World!');
+import express,{Request,Response,NextFunction} from "express";
+const app = express();
+
+app.get('/status',(req:Request, res:Response, next:NextFunction)=>{
+    res.status(200).send({for:'bar'});
+} );
+
+app.listen(3000, ()=>{
+    console.log('Aplicação executando na porta 3000');
+})
