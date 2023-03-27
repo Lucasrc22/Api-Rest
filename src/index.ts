b595f1b0400a6d90b4ca5f823cb5ef18ebb0df7b
Lucas Ribeiro Costa <lucasrios22@hotmail.com>
2023-03-27 11:06:06 -0300

    aprimoramento da API

--- src/index.ts
+++ src/index.ts
@@ -1,10 +1,18 @@
 import express,{Request,Response,NextFunction} from "express";
+import statusRoute from "./routes/status.route";
+import usersRoute from "./routes/users.route";
 const app = express();
 
+//  Configurações da aplicação
+app.use(express.json());
+app.use(express.urlencoded({extended: true}));
+//configuração das rotas
+app.use(usersRoute);
+app.use(statusRoute);
 app.get('/status',(req:Request, res:Response, next:NextFunction)=>{
-    res.status(200).send({for:'bar'});
-} );
-
+    res.status(200).send({for:'sucesso'});
+});
+//inicialização do servidor 
 app.listen(3000, ()=>{
     console.log('Aplicação executando na porta 3000');
-})
+});
---
+++ src/routes/status.route.ts
@@ -0,0 +1,9 @@
+import { Router, Response, Request, NextFunction } from "express";
+import { StatusCodes } from "http-status-codes";
+
+const statusRoute = Router();
+
+statusRoute.get('/status',(req:Request,res:Response,next:NextFunction)=>{
+    res.send(StatusCodes.OK);
+});
+export default statusRoute;
